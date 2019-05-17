40ff57caa741472d27c94060234df4b9ee884540
DevilMayCry2333 <1712363499@qq.com>
2019-05-17 23:07:31 +0800

    第一次伟大的提交

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,61 +1,43 @@
-//package com.example.myapplication
-//
-//import android.os.Bundle
-//import android.support.design.widget.Snackbar
-//import android.support.v7.app.AppCompatActivity;
-//import android.view.Menu
-//import android.view.MenuItem
-//
-//import kotlinx.android.synthetic.main.activity_main.*
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        setSupportActionBar(toolbar)
-//        fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//        }
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        return when (item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
-//}
-
 package com.example.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.myapplication.R;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+
+public class MainActivity extends AppCompatActivity {
+    private TextView res ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        res = findViewById(R.id.res);
+        Bundle bundle = this.getIntent().getExtras();
+        String resText = bundle.getString("res");
+        try {
+            JSONObject token = new JSONObject(resText);
+            if(token.has("token")){
+               TextView tokenTextView = findViewById(R.id.token);
+                tokenTextView.setText(token.getString("token"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // 获取解析结果
@@ -75,13 +56,16 @@ public class MainActivity extends AppCompatActivity {
             if (result.getContents() == null) {
                 Toast.makeText(this, "取消扫描", Toast.LENGTH_LONG).show();
             } else {
+                int i = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+                Intent intent = new Intent(MainActivity.this,Detail.class);
+                intent.putExtra("result",result.getContents());
+                startActivity(intent);
+
                 Toast.makeText(this, "扫描内容:" + result.getContents(), Toast.LENGTH_LONG).show();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
-
-
 }
-
