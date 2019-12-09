c9c2a364d3a16828beb9efc6f062c2b8296fc9e6
Abigale666 <1015474413@qq.com>
2019-12-09 14:19:03 +0800

    最新

--- src/pages/newDeadTree/newDeadTree.ts
+++ src/pages/newDeadTree/newDeadTree.ts
@@ -495,6 +495,7 @@ export class DeadtreePage {
 
 
     async ionViewDidLoad(){
+        localStorage.removeItem('deadCache');
         this.threePhotos = false;
         this.canSubmit = true;
         var that = this;
